f691ccce9ca309a4fe8fba0d0907e95f9168a55e
lilowl <alyssiamari@yahoo.fr>
2021-11-18 22:25:46 +0100

    add create user & add group

--- setupInstances.py
+++ setupInstances.py
@@ -5,18 +5,28 @@ import time
 
 ec2 = boto3.resource('ec2', region_name="eu-central-1")
 user = str(sys.argv[1])
+userGroup = str(sys.argv[2])
 SecurityGroupIds='sg-045de45c2834b1b04' # A changer en fonction de l'utilisateur du script
 
-# Function definition
+######## Function definition ########
 def nameAvailable(userName) :
     unavailableUserNames = [f.split('.')[0] for f in os.listdir("/tmp/") if (os.path.isfile(f"/tmp/{f}") and (".pem" in f))]
     return not (userName in unavailableUserNames)
 
+def create_user(userName):
+    iam = boto3.client("iam")
+    iam.create_user(UserName=userName)
+
+def add_user_to_group(userName, group_name):
+    iam = boto3.client('iam') # IAM low level client object
+    iam.add_user_to_group(
+        UserName=userName,
+        GroupName=group_name
+    )
+
 def create_key_pair(userName):
     key_pair = ec2.create_key_pair(KeyName=userName)
-
     private_key = key_pair.key_material
-
     # change permission to file with 400 permissions
     with os.fdopen(os.open(f"/tmp/{userName}.pem", os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
         handle.write(private_key)
@@ -43,12 +53,20 @@ def create_instance(keyName):
     )
     return res
 
-# Apply function
+######## Apply function ########
 if not nameAvailable(user) : 
     print("User name is taken") 
     exit()
+
+create_user(user)
+print(f"***** User {user} has been created *****")
+
+add_user_to_group(user, userGroup)
+print(f"***** User {user} has been put into group {userGroup} *****")
+
 create_key_pair(user)
 print("***** Key pair create *****")
+
 instances=create_instance(user)
 print("***** Instance EC2 create *****")
 
